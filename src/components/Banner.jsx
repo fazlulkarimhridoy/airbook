ff02d8cfbbd0b01f3237e294c03b0bd29124d6dc
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-03-19 20:43:57 +0600

    polished

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,13 @@
 import BannerImage from "../assets/banner.png"
+import { FaRegCircle, FaSearch } from "react-icons/fa";
+
 const Banner = () => {
     const style = {
         backgroundImage: `url(${BannerImage})`,
-        backgroundSize: "fit",
+        backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        height: "70vh",
+        height: "80vh",
         width: "99vw",
         backgroundAttachment: "fixed",
         overflow: "hidden"
@@ -17,17 +19,75 @@ const Banner = () => {
                     Welcome to <span className="text-[#006CE4]">AirBook</span>
                 </h2>
                 <div className="bg-white mt-5 p-20 w-5/6 rounded-lg">
-                    <div>
+                    <div className="flex items-center justify-between">
                         <div className="flex items-center gap-3">
                             <button className="bg-gray-200 flex items-center gap-2 px-4 py-1.5 rounded-lg">
-                                <div className="size-[20px] border border-gray-600 rounded-full bg-gray-200"></div>
+                                <FaRegCircle color="gray"></FaRegCircle>
                                 <h2 className="text-sm font-medium">One Way</h2>
                             </button>
                             <button className="bg-[#006CE4] flex items-center gap-2 px-4 py-1.5 rounded-lg text-white">
-                                <div className="size-[20px] border border-gray-600 rounded-full bg-gray-200"></div>
+                                <div className="size-[16px] m-1 rounded-full bg-gray-200">
+                                </div>
                                 <h2 className="text-sm font-medium">Round Trip</h2>
                             </button>
                         </div>
+                        <div className="flex items-center gap-3">
+                            <button className="bg-[#EBF0F5] text-[#006CE4] flex items-center gap-2 px-4 py-1.5 rounded-lg">
+                                1 Traveller
+                            </button>
+                            <button className="bg-[#EBF0F5] text-[#006CE4] flex items-center gap-2 px-4 py-1.5 rounded-lg text-white">
+                                Economy
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mt-5 flex items-center justify-between gap-3">
+                        <div className="flex items-center gap-2 border border-gray-400 rounded-md px-4 py-2 w-full">
+                            <p className="text-sm font-semibold border-r-2 border-gray-400">DAC</p>
+                            <p className="text-sm font-medium flex flex-col">
+                                <span>
+                                    Dhaka
+                                </span>
+                                <span className="text-[10px]">
+                                    Hazrat Shahjalal Internatio
+                                </span>
+                            </p>
+                        </div>
+                        <div className="flex items-center gap-2 border border-gray-400 rounded-md px-4 py-2 w-full">
+                            <p className="text-sm font-semibold border-r-2 border-gray-400">DAC</p>
+                            <p className="text-sm font-medium flex flex-col">
+                                <span>
+                                    Dhaka
+                                </span>
+                                <span className="text-[10px]">
+                                    Hazrat Shahjalal Internatio
+                                </span>
+                            </p>
+                        </div>
+                        <div className="flex items-center gap-2 border border-gray-400 rounded-md px-4 py-2 w-full">
+                            <p className="text-sm font-semibold border-r-2 border-gray-400">DAC</p>
+                            <p className="text-sm font-medium flex flex-col">
+                                <span>
+                                    Dhaka
+                                </span>
+                                <span className="text-[10px]">
+                                    Hazrat Shahjalal Internatio
+                                </span>
+                            </p>
+                        </div>
+                        <div className="flex items-center gap-2 border border-gray-400 rounded-md px-4 py-2 w-full">
+                            <p className="text-sm font-semibold border-r-2 border-gray-400">DAC</p>
+                            <p className="text-sm font-medium flex flex-col">
+                                <span>
+                                    Dhaka
+                                </span>
+                                <span className="text-[10px]">
+                                    Hazrat Shahjalal Internatio
+                                </span>
+                            </p>
+                        </div>
+                        <button className="bg-[#FFB700] px-8 py-5 rounded-md">
+                            <FaSearch color="white"></FaSearch>
+                        </button>
                     </div>
                 </div>
             </div>
